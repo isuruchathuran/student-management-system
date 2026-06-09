bbc5a0432f31dbce99838d73f4c8411dc3fb8678
isuruchathuran <isuruchathuran24@gmail.com>
2026-06-09 10:30:08 +0530

    Setup migration files and connect DB

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[AdminController::class,'dashboard'])->name('admin.dashboard');
 Route::get('/student',[StudentController::class,'student'])->name('admin.student');
 Route::get('/about',[AdminController::class,'about'])->name('admin.about');
+
